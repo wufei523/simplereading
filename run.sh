daeaf12c194b0edcd153ea1e2967fbef75b539c5
wufei523 <wufei523@gmail.com>
2016-06-03 09:34:15 -0700

    works on bluemix

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-SERVER_PORT="$PORT";
+#!/bin/bash
+if [ -z "$VCAP_APP_PORT" ];
+then SERVER_PORT=5000;
+else SERVER_PORT="$VCAP_APP_PORT";
 fi
-echo port is $SERVER_PORT
+echo port is------------------- $SERVER_PORT
+#python manage.py syncdb --noinput
 python manage.py runserver 0.0.0.0:$SERVER_PORT
+
+
+#SERVER_PORT="$PORT";
+#fi
+#echo port is $SERVER_PORT
+#python manage.py runserver 0.0.0.0:$SERVER_PORT
